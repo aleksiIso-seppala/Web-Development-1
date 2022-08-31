aabe73ec9653a7772ac4f766cd47d44f38c6a3f7
Aleksi Iso-Seppala <aleksi.iso-seppala@tuni.fi>
2022-08-31 16:07:57 +0300

    deleted 01_tools

--- exercises/01_tools/hellonode.js
+++
@@ -1,9 +0,0 @@
-const { last } = require("lodash");
-const _ = require("lodash");
-    const ver = _.VERSION;
-    console.log(ver);
-    
-    function hithere(array){
-        return (head(array)+last(array));
-    }
-    module.exports = hithere;
