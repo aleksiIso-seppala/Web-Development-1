07fde00bc84154274ff2c4670b28be8cc42ae82a
Aleksi Iso-Seppala <aleksi.iso-seppala@tuni.fi>
2022-09-08 22:59:20 +0300

    API testing done

---
+++ exercises/02_http/2.7-api-testing/index.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = http.createServer((request, response) => {
+
+  //TODO: implement sending responds to requests for XML, TXT, and */* below, handling JSON provided as an example.
+  if(request.url === '/classical'){
+    response.statusCode = 200;
+    readFileSendResponse('homer.html','text/html',response);
+  }
+  else if(request.url === '/dystopy'){
+    response.statusCode= 200;
+    readFileSendResponse('bradbury.html','text/html',response);
+  }
+  else if(request.url === '/'){
+    response.statusCode = 200;
+    readFileSendResponse('index.html','text/html',response);
+  }
+  else{
+    response.statusCode = 404;
+    response.statusMessage = 'Requested content not found';
+    response.end();
+  }
+}).listen(3000);
+
+const readFileSendResponse = (fileName, contentType, response) => {
+  fs.readFile(path.resolve(fileName), function(error, file) {
+    if (error) {
+      response.writeHead(404);
+      response.write('An error occured: ', error);
+    } else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.write(file);
+    }
+    response.end();
+  })
+}
