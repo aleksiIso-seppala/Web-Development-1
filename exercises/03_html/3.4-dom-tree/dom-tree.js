fc8f903626b37f529338c1e98896c00cfc2b852b
Aleksi <aleksi.iso-seppala@tuni.fi>
2022-09-22 21:21:36 +0300

    dom-tree valmis

---
+++ exercises/03_html/3.4-dom-tree/dom-tree.js
@@ -0,0 +1,24 @@
+
+var newList = document.createElement("ul");
+newList.className = "list";
+
+var newli = document.createElement("li");
+
+var newLink = document.createElement("a");
+newLink.href = "http://localhost:3000/";
+newLink.text = "Localhost";
+
+var navUl = document.querySelector("nav");
+
+navUl.appendChild(newList);
+newList.appendChild(newLi);
+newLi.appendChild(newLink);
+
+var firstLi = document.createElement("li");
+firstLi.textContent = "Item 0";
+
+var olPointer = document.getElementById("ol");
+olPointer.insertBefore(firstLi,olPointer.firstChild);
+
+var todo = document.getElementById("todo");
+todo.classList.remove("navi");
