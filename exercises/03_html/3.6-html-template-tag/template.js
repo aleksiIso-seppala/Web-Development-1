95f35e01dfb0af817db1af92a7d5209708853d1e
Aleksi Iso-Seppala <aleksi.iso-seppala@tuni.fi>
2022-09-23 17:27:46 +0300

    valmis template

---
+++ exercises/03_html/3.6-html-template-tag/template.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("form");
+const contacts = document.querySelector("div");
+const contactTemplate = document.querySelector("template");
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener('click', (event) =>{
+    event.preventDefault();
+    var formData = new FormData(form);
+    var name = formData.get("name");
+    var email = formData.get("email");
+    var homepage = formData.get("homepage");
+
+    let clone = contactTemplate.content.cloneNode(true);
+    clone.querySelector("h2").innerHTML = name;
+    clone.querySelector("p.email").innerHTML = email;
+    clone.querySelector("p.homepage").querySelector("a").href = homepage;
+    clone.querySelector("p.homepage").querySelector("a").innerHTML = homepage;
+    contacts.append(clone);
+});
