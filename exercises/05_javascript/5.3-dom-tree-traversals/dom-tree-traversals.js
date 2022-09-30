312dd5798f79cca37764428e57e481a7a4f7f6ba
Aleksi Iso-Seppala <aleksi.iso-seppala@tuni.fi>
2022-09-30 18:32:18 +0300

    valmis ohjelma testi

---
+++ exercises/05_javascript/5.3-dom-tree-traversals/dom-tree-traversals.js
@@ -0,0 +1,16 @@
+
+
+function CountNestedElements(){
+    var TopElement = document.querySelector('ul');
+        var listOfElements = TopElement.getElementsByTagName('li');
+        
+        for(var i=0; i<listOfElements.length;i++){
+            var current = listOfElements[i];
+            if(current.querySelector('ul') == null){
+                continue;
+            }
+            var count = current.querySelector('ul').getElementsByTagName('li').length;
+            var name = current.innerHTML;
+            current.innerHTML = name + " (" + count + ")";
+        }
+    }
