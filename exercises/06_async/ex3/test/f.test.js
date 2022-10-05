ae8ac46827f490d41d3383f10768b38f8672f7f1
Aleksi <aleksi.iso-seppala@tuni.fi>
2022-10-05 16:55:22 +0300

    fixed issues with f.test

--- exercises/06_async/ex3/test/f.test.js
+++ exercises/06_async/ex3/test/f.test.js
@@ -1,5 +1,6 @@
 const expect = require("chai").expect;
 
+const { assert } = require("chai");
 const f = require("../f_skeleton");
 
 describe("thenable", () => {
@@ -9,7 +10,13 @@ describe("thenable", () => {
     // This test can be implemented with either Promises or with async / await, as shown on that page.
     // Use expect() and to.equal()
     // to make sure that the resolved value is 👍
-    throw "Not yet implemented!";
+
+    Promise.resolve(f.thenable)
+    .catch((err) => err)
+    .catch((err) => {
+      expect(err).to.equal("👍");
+      done();
+    });
   });
 });
 
